0cd10bda481205b04923025aed7eb5dd67238d97
wengchengjian <473991883>
2023-06-01 14:20:09 +0800

    feat: 数据库test命令行工具

---
+++ src/cli.rs
@@ -0,0 +1,26 @@
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "ckd",
+    about = "A command line tool to load data into ClickHouse."
+)]
+pub struct Cli {
+    #[structopt(long)]
+    pub host: Option<String>,
+
+    #[structopt(long)]
+    pub port: Option<u16>,
+
+    #[structopt(long)]
+    pub user: Option<String>,
+
+    #[structopt(long)]
+    pub password: Option<String>,
+
+    #[structopt(long)]
+    pub batch: Option<usize>,
+
+    #[structopt(long)]
+    pub print: Option<usize>,
+}

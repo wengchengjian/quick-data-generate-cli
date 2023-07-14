398750136f215986210679644b567b4085166c23
wengchengjian <473991883>
2023-07-14 17:21:43 +0800

    feat: 添加tauri桌面应用初始化

---
+++ ckd-ui/src-tauri/src/main.rs
@@ -0,0 +1,28 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::{CustomMenuItem, Manager, Menu, MenuItem, Submenu};
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+fn close_splashscreen(window: tauri::Window) {
+    // 关闭启动视图
+    if let Some(splashscreen) = window.get_window("splashscreen") {
+        splashscreen.close().unwrap();
+    }
+    // 展示主视图
+    window.get_window("main").unwrap().show().unwrap();
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![close_splashscreen])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}

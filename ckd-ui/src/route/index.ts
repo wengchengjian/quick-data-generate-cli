82eeab4bc032808197bccfc029036b9e10a329b6
wengchengjian <473991883>
2023-08-03 10:25:15 +0800

    delete github action

--- ckd-ui/src/route/index.ts
+++ ckd-ui/src/route/index.ts
@@ -8,7 +8,6 @@ import { createRouter, createWebHashHistory } from "vue-router";
 
 // 2. 定义一些路由
 // 每个路由都需要映射到一个组件。
-// 我们后面再讨论嵌套路由。
 const routes = [
     { path: '/', redirect: '/dashboard' },
     { path: '/datasource/:id(\\d+)?', component: DataSource },
